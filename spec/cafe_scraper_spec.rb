e129a348c923bd8b32b67e83475ec52967b25dc8
mogya <mogya@mogya.com>
2016-02-04 16:50:39 +0900

    remove unused test.

--- spec/cafe_scraper_spec.rb
+++ spec/cafe_scraper_spec.rb
@@ -4,8 +4,4 @@ describe CafeScraper do
   it 'has a version number' do
     expect(CafeScraper::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
